7ba5cf98b837ead2683116b676b3f52fa263aa2b
honger05 <honger05@126.com>
2014-11-21 16:14:40 +0800

    v1.0.0

---
+++ sea-modules/sys-back/system/1.0.0/system-debug.js
@@ -0,0 +1,4 @@
+define("sys-back/system/1.0.0/system-debug", [ "$-debug" ], function(require, exports, module) {
+    var $ = require("$-debug");
+    alert($);
+});
---
+++ sea-modules/sys-back/system/1.0.0/system.js
@@ -0,0 +1,1 @@
+define("sys-back/system/1.0.0/system",["$"],function(a){var b=a("$");alert(b)});
